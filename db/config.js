a5b08ad5e27db1862d1119a21658df8a1d95f472
Chris Harding <christopher.harding@gmx.com>
2018-03-26 10:55:33 -0400

    hero ..

--- db/config.js
+++ db/config.js
@@ -10,9 +10,9 @@ let db;
 
 if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
   db = pgp({
-    database: 'gamblr' || process.env.PG_DATABASE,
-    port: 5432 || process.env.PG_PORT,
-    host: 'localhost' || process.env.PG_HOST,
+    database: process.env.PG_DATABASE || 'gamblr',
+    port: process.env.PG_PORT || 5432,
+    host: process.env.PG_HOST || 'localhost',
   });
 } else if (process.env.NODE_ENV === 'production') {
   db = pgp(process.env.DATABASE_URL);
